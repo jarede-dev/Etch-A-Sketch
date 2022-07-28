a447dcac3e9217976f56c159dc9fb07a9a7fa2f9
Jared Ramon Elizan <elizanjaredr@gmail.com>
2022-07-28 08:15:52 +0800

    made a loop to make squares

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let cont = document.createElement("div");
 let sqr = document.createElement("div");
 
 // add id and class
-let a = cont.setAttribute("id", "container");
-let b = sqr.setAttribute("class", "square");
+let contId = cont.setAttribute("id", "container");
+let sqrClass = sqr.setAttribute("class", "square");
 
 // select the elements
 let container = document.querySelector("#container");
@@ -13,3 +13,15 @@ let square = document.querySelector(".square");
 // output to DOM
 cont.appendChild(sqr);
 document.body.appendChild(cont);
+
+// declare i
+let i = 0;
+
+// loop square class
+for(i; i <= 254; i++){
+    const square = document.createElement("div");
+    square.classList.add("square", "square-" + i );
+    cont.appendChild(square);
+    console.log(square);
+}
+
