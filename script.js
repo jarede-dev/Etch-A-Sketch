9c7945e35d22851ece437963079eaf89231b0111
Jared Ramon Elizan <elizanjaredr@gmail.com>
2022-07-31 16:32:39 +0800

    made btn and input variables and selected them

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let square = document.querySelector(".square");
 let container = document.querySelector("#container");
 let mainContainer = document.querySelector("#mainContainer");
 
+let btn = document.querySelector("#askNumber");
+let input = document.querySelector("#input");
+
 //append and output to DOM
 container.appendChild(square);
 
@@ -14,7 +17,7 @@ document.body.appendChild(mainContainer);
 let i = 0;
 
 // loops square class
-for(i; i <= 256 - 2; i++){
+for(i; i <= 10000 - 2; i++){
     const square = document.createElement("div");
     square.classList.add("square", "square-" + i );
     container.appendChild(square);
