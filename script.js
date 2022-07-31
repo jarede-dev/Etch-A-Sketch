c76283c7237ecae8df40ccc6357931bb4547acec
Jared Ramon Elizan <elizanjaredr@gmail.com>
2022-07-31 08:36:29 +0800

    transfered created elements into html and selected the containers

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-// create divs
-let cont = document.createElement("div");
-let sqr = document.createElement("div");
-
-// add id and class
-let contId = cont.setAttribute("id", "container");
-let sqrClass = sqr.setAttribute("class", "square");
-
-// select the elements
-let container = document.querySelector("#container");
 let square = document.querySelector(".square");
 
-// outputs to DOM
-cont.appendChild(sqr);
-document.body.appendChild(cont);
+let container = document.querySelector("#container");
+let mainContainer = document.querySelector("#mainContainer");
+
+//append and output to DOM
+container.appendChild(square);
+
+mainContainer.appendChild(container);
+
+document.body.appendChild(mainContainer);
 
 // declares i
 let i = 0;
@@ -21,8 +17,8 @@ let i = 0;
 for(i; i <= 256 - 2; i++){
     const square = document.createElement("div");
     square.classList.add("square", "square-" + i );
-    cont.appendChild(square);
+    container.appendChild(square);
     //console.log(square);
 }
 
-//console.log(cont);
+//console.log(container);
